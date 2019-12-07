830c8ad3c83703cc143b037dba330c877dcb5b80
shibacow <shibacow@gmail.com>
2019-12-07 15:40:42 +0900

    ch data

--- get_info.py
+++ get_info.py
@@ -5,44 +5,40 @@ from mog_op import MongoOp
 def execute_sql():
     client = bigquery.Client()
     query_job = client.query("""
-SELECT b.id as tweet_id,
-a.screen_name AS screen_name,
-a.user_id AS user_id,
-b.created_at AS created_at,
-b.mfv AS max_fav,
-b.mrt AS max_rt,
-b.text AS text
-FROM 
-(SELECT id as user_id,screen_name
-FROM twitter.temp_user
-GROUP BY id,screen_name
-) a
-JOIN 
-(select 
-id,user_id,created_at,text,max(favorite_count) as mfv,
-max(retweet_count) as mrt
+select 
+id AS tweet_id,
+user_id,created_at,text,max(favorite_count) as max_fav,
+max(retweet_count) as max_rt
 from 
 twitter.temp_retweeted_status
 group by id,user_id,created_at,text
-having mfv > 5000 or mrt > 5000
-) b
-ON a.user_id=b.user_id
+having max_fav > 5000 or max_rt > 5000
 """)
     return query_job.result()
 def insert_mp(mp,results):
     rlist=[]
-    for r in results:
+    for i,r in enumerate(results):
+        url = 'https://twitter.com/_/status/{}'.format(r.tweet_id)
         rlist.append(dict(
             tweet_id = r.tweet_id,
-            screen_name = r.screen_name,
             user_id = r.user_id,
             created_at = r.created_at,
             max_fav = r.max_fav,
             max_rt = r.max_rt,
-            text = r.text
+            text = r.text,
+            url=url
         ))
-    result  = mp.col.insert_many(rlist)
-    print(result)
+        if i>0 and i%10000 == 0:
+            if(rlist):
+                result  = mp.col.insert_many(rlist)
+                print('i={} result={}'.format(i,result))
+                rlist=[]
+    if(rlist):
+        result  = mp.col.insert_many(rlist)
+        print('i={} result={}'.format(i,result))
+        rlist=[]
+
+    
 def main():
     mp = MongoOp('localhost')
     results = execute_sql()
