8a472ae5a24ae2041d0dd5cb945f152d31454236
shibacow <shibacow@gmail.com>
2019-12-08 14:07:12 +0900

    add fpath

--- get_img.py
+++ get_img.py
@@ -5,17 +5,47 @@ import get_png
 import logging
 logging.basicConfig(level=logging.INFO)
 
-def main():
+def add_fpath():
+    mp=MongoOp('localhost')
+    k='max_rt'
+    cnt=mp.col.count({k:{'$gt':50_000}})
+    for i,a in enumerate(mp.col.find({k:{'$gt':50_000}})):
+        url = a['url']
+        (r,path) = get_png.check_path('static',url)
+        up_elm={'$set':{'fpath':path}}
+        if not r:
+            up_elm={'$set':{'fpath':'NotFound'}}
+        col_r = mp.col.update_one({'_id':a['_id']},up_elm)
+        msg='k={} cnt={} i={} url={} path={} col_r={}'.format(k,cnt,i,url,path,col_r)
+        logging.info(msg)
+def get_img():
     mp=MongoOp('localhost')
     driver = get_png.factory()
-    for k in ('max_rt','max_fav'):
-        cnt=mp.col.count({k:{'$gt':50_000}})
-        for i,a in enumerate(mp.col.find({k:{'$gt':50_000}})):
-            url = a['url']
-            get_png.save_png(driver,url,'static',logging)
-            msg='k={} cnt={} i={} url={}'.format(k,cnt,i,url)
+    k='max_fav'
+    cond={k:{'$gt':50_000},'fpath':{'$exists':False}}
+    cnt=mp.col.count(cond)
+    for i,a in enumerate(mp.col.find(cond)):
+        url = a['url']
+        if 'fpath' in a:continue
+        (r,path) = get_png.check_path('static',url)
+        if r:
+            up_elm={'$set':{'fpath':path}}
+            mp.col.update_one({'_id':a['_id']},up_elm)
+            msg='file exists={}'.format(path)
             logging.info(msg)
+            continue
+        result = get_png.save_png(driver,url,'static',logging)
+        msg='k={} cnt={} i={} url={} fpath={}'.format(k,cnt,i,url,result)
+        logging.info(msg)
+        up_elm={'$set':{'fpath':path}}
+        if not result:
+            up_elm={'$set':{'fpath':'NotFound'}}
+        col_r = mp.col.update_one({'_id':a['_id']},up_elm)
     get_png.close(driver)
+
+def main():
+    #add_fpath()
+    get_img()
 if __name__=='__main__':main()
 
     
--- get_png.py
+++ get_png.py
@@ -16,22 +16,34 @@ def factory():
     options.add_argument('--window-size=1440,2560')
     driver = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver", chrome_options=options);
     return driver
+
+def check_path(base_path,url):
+    bname=url.split('/')[-1][:4]
+    tid=url.split('/')[-1]
+    bname_path=base_path + os.sep + bname
+    fpath = bname_path+os.sep+tid+'.png'
+    if os.path.isfile(fpath):
+        return True,fpath
+    else:
+        return False,fpath
+    
+def get_path(base_path,url):
+    if not os.path.isdir(base_path):
+        os.mkdir(base_path)
+    bname=url.split('/')[-1][:4]
+    tid=url.split('/')[-1]
+    bname_path=base_path + os.sep + bname
+    if not os.path.isdir(bname_path):
+        os.mkdir(bname_path)
+    return bname_path+os.sep+tid+'.png'
+
 def save_png(driver,url,base_path,logging):
-    def get_path(url):
-        if not os.path.isdir(base_path):
-            os.mkdir(base_path)
-        bname=url.split('/')[-1][:4]
-        tid=url.split('/')[-1]
-        bname_path=base_path + os.sep + bname
-        if not os.path.isdir(bname_path):
-            os.mkdir(bname_path)
-        return bname_path+os.sep+tid+'.png'
     driver.get(url)
     driver.implicitly_wait(3.0)
     try:
         png = driver.find_element_by_xpath('/html/body/div[30]/div[2]/div[3]/div/div/div[1]/div[1]')
         if png:
-            fpath = get_path(url)
+            fpath = get_path(base_path,url)
             png = png.screenshot_as_png
             with open(fpath, "wb") as f:
                 f.write(png)
